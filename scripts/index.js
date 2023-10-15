4434277280328eca86ce0b98c60a6803f4d6ad6c
pasha-log <pasha.log@gmail.com>
2023-10-15 13:48:46 -0700

    added mountain schedule toggle functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+$(document).ready(function() {
+	$('#mountain-one, #mountain-two').click(function() {
+		$('#mountain-one, #mountain-two').toggleClass('active inactive');
+		$('#tab-one-image, #tab-two-image').toggleClass('active-tab-image inactive-tab-image');
+		$('#schedule-section').toggleClass('active-section inactive-section');
+	});
+});
