f7cc646b21aaf4274c7d5bcd314a64696b7ab8c7
pasha-log <pasha.log@gmail.com>
2023-10-15 17:26:02 -0700

    added mobile accordion for schedule tabs with bootstrap

--- scripts/index.js
+++ scripts/index.js
@@ -6,4 +6,13 @@ $(document).ready(function() {
 		$('#tab-one-image, #tab-two-image').toggleClass('active-tab-image inactive-tab-image');
 		$('#schedule-section').toggleClass('active-section inactive-section');
 	});
+
+	$('#accordion-button-one').click(function() {
+		$('#accordion-button-one').toggleClass('"" collapsed');
+		$('#flush-collapseOne').toggleClass('"" show');
+	});
+	$('#accordion-button-two').click(function() {
+		$('#accordion-button-two').toggleClass('"" collapsed');
+		$('#flush-collapseTwo').toggleClass('"" show');
+	});
 });
